293152d0f785e882e4f95647caaa9433547e7505
Jojozzc <zzc185@foxmail.com>
2021-01-10 15:38:11 +0800

    add pause

--- rename_main.py
+++ rename_main.py
@@ -27,6 +27,7 @@ def rename_mode(config_parser):
     file_size_threshold_in_bytes = config_parser.getfloat('CONFIG', 'file.threshold.bytes')
     start_index = config_parser.getint('CONFIG', 'index.start', fallback=1)
     print("root path:{}".format(root_path))
+    print()
     rename_core.rename_batch(root_path=root_path,
                              prefix=prefix,
                              small_file_prefix=small_file_prefix,
@@ -38,6 +39,8 @@ def rename_mode(config_parser):
 
 def recall_mode(config_parser):
     root_path = config_parser.get('CONFIG', 'target.path')
+    print("root path:{}".format(root_path))
+    print()
     file_suffix_set = set(parse_list(config_parser.get('CONFIG', 'file.suffix.set')))
     ignore_case = config_parser.getboolean('CONFIG', 'case.ignore', fallback=True)
     prefix_remove_mark = config_parser.get('CONFIG', 'prefix.remove.mark')
@@ -45,21 +48,31 @@ def recall_mode(config_parser):
     rename_core.recall(root_path=root_path, file_suffix_set=file_suffix_set, ignore_case=ignore_case, prefix_remove_mark=prefix_remove_mark)
 
 if __name__ == "__main__":
-    _curPath = os.path.abspath(os.path.dirname(__file__))
-    _rootPath = os.path.split(_curPath)[0]
-    sys.path.append(_rootPath)
+    try:
+        _curPath = os.path.abspath(os.path.dirname(__file__))
+        _rootPath = os.path.split(_curPath)[0]
+        sys.path.append(_rootPath)
 
+        config_path = u"./config.ini"
+        config_parser = configparser.ConfigParser()
+        config_parser.read(config_path, encoding='utf-8')
 
-    config_path = u"./config.ini"
-    config_parser = configparser.ConfigParser()
-    config_parser.read(config_path, encoding='utf-8')
-
-    if len(sys.argv) <= 1:
-        rename_mode(config_parser)
-    elif sys.argv[1] == '-z':
-        print('撤回中...')
-        recall_mode(config_parser)
-        print('撤回成功')
-    else:
-        print('未知参数:{}'.format(sys.argv))
+        if len(sys.argv) <= 1:
+            print('开始批量修改...')
+            print('===========================================')
+            rename_mode(config_parser)
+            print('===========================================')
+            print('批量修改成功')
+        elif sys.argv[1] == '-z':
+            print('撤回中...')
+            print('===========================================')
+            recall_mode(config_parser)
+            print('===========================================')
+            print('撤回成功')
+        else:
+            print('未知参数:{}'.format(sys.argv))
+    except Exception as e:
+        print('执行出错:', e)
+    finally:
+        os.system('pause')
 
